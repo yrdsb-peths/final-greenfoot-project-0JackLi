87a1fa8e19b69834554eb76ab32b1dbba51c5803
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-12-11 09:10:56 -0500

    added background

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,26 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        createBackground();
+        addLine();
+    }
+    
+    private void createBackground()
+    {
+        Background background = new Background();
+        GreenfootImage image = background.getImage();
+        image.scale(getWidth(), getHeight());
+        setBackground(image);
+    }
+    
+    private void addLine()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            Line line = new Line();
+            GreenfootImage image = line.getImage();
+            image.scale(900, 300);
+            addObject(line, getWidth()/2, getHeight()/8 * i);
+        }
     }
 }
