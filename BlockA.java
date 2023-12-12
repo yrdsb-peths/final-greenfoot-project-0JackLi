8d6d73948887e8e1dd32b6df7413d1f052e9e169
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-12-11 23:19:20 -0500

    added some picture

---
+++ BlockA.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BlockA here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BlockA extends Actor
+{
+    /**
+     * Act - do whatever the BlockA wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
