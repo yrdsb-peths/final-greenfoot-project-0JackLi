2ac521c3e24c95a7b1a034c60bd2bb5ef0ed3512
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-12-12 08:06:21 -0500

    Update project.greenfoot

--- MyWorld.java
+++ MyWorld.java
@@ -16,11 +16,12 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(400, 600, 1);        
         createBackground();
         addLine();
+        addCha();
     }
-    
+
     private void createBackground()
     {
         Background background = new Background();
@@ -28,15 +29,47 @@ public class MyWorld extends World
         image.scale(getWidth(), getHeight());
         setBackground(image);
     }
-    
+
     private void addLine()
     {
-        for(int i = 0; i < 9; i++)
+        for(int i = 0; i < 16; i++)
         {
             Line line = new Line();
             GreenfootImage image = line.getImage();
-            image.scale(900, 300);
-            addObject(line, getWidth()/2, getHeight()/8 * i);
+            image.scale(900, 100);
+            addObject(line, getWidth()/2, getHeight()/15 * i);
+        }
+
+        for(int i = 0; i < 11; i++)
+        {
+            Line2 line2 = new Line2();
+            GreenfootImage image = line2.getImage();
+            image.scale(100, 900);
+            addObject(line2, getWidth()/10 * i, getHeight()/2);
         }
     }
+
+    
+    private void addCha()
+    {
+        BlockA ch0;
+        BlockB ch1;
+        BlockC ch2;
+        BlockD ch3;
+        BlockE ch4;
+        BlockF ch5;
+        BlockG ch6;
+        Actor[] actor = { ch0 = new BlockA(),
+                ch1 = new BlockB(),
+                ch2 = new BlockC(),
+                ch3 = new BlockD(), 
+                ch4 = new BlockE(),
+                ch5 = new BlockF(),
+                ch6 = new BlockG()};
+        for(int i = 0; i < actor.length; i++)
+        {
+            addObject(actor[i], 40 * i + 22, 60);
+        }
+    }
+
 }
